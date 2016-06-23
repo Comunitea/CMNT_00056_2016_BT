ce2775beed7285eb0e59b82bc7b97e4f74593d5e
jesusVMayor <jesus@comunitea.com>
2016-06-23 16:04:22 +0200

    magento_sale_store: Añadida tienda a importación de prestashop.

---
+++ project-addons/magento_sale_store/models/sale_prestashop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.addons.prestashoperpconnect.unit.mapper import SaleOrderMapper
+from openerp.addons.prestashoperpconnect.backend import prestashop
+from openerp.addons.connector.unit.mapper import mapping
+
+
+@prestashop(replacing=SaleOrderMapper)
+class SaleOrderMapperAddStore(SaleOrderMapper):
+
+    @mapping
+    def store_id(self, record):
+        import ipdb; ipdb.set_trace()
+        binder = self.binder_for('prestashop.shop')
+        shop_store = binder.to_openerp(record['id_shop'])
+        sale_store = self.env['prestashop.shop'].browse(shop_store).store_id
+        return {'sale_store_id': sale_store.id}
