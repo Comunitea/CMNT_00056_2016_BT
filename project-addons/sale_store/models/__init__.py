0c116eca0a5aa1deab176bfbb65c93660e2fa429
Omar <omarcs7r@gmail.com>
2016-09-22 18:01:13 +0200

    [REF] magento_sale_store: Se extrae el modleo de tiendas a un móudlo independiente para que no dependa de los móudlos de ecommerce

--- project-addons/sale_store/models/__init__.py
+++ project-addons/sale_store/models/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # © 2016 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import magento_store
-from . import prestashop_store
+
 from . import sale
-from . import sale_prestashop
+from . import stock
+from . import account_invoice
+from . import partner
