727ba7228c4da5787d04fd6526d94e733b3e0d50
Omar <omarcs7r@gmail.com>
2016-09-29 20:53:20 +0200

    [ADD] on_changes_without_code: Evitamos que se pasa más datos que el nombre del prodicto en los on_changes de ventas y factura

---
+++ project-addons/on_changes_without_code/models/invoice.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class AccountInvoiceLine(models.Model):
+
+    _inherit = "account.invoice.line"
+
+    @api.multi
+    def product_id_change(self, product, uom_id, qty=0, name='',
+                          type='out_invoice', partner_id=False,
+                          fposition_id=False, price_unit=False,
+                          currency_id=False, company_id=None):
+        res = super(AccountInvoiceLine, self).\
+            product_id_change(product, uom_id, qty=qty, name=name,
+                              type=type, partner_id=partner_id,
+                              fposition_id=fposition_id, price_unit=price_unit,
+                              currency_id=currency_id, company_id=company_id)
+        if product and res.get('value') and res['value'].get('name'):
+            part = self.env['res.partner'].browse(partner_id)
+            if part.lang:
+                self = self.with_context(lang=part.lang)
+            product_obj = self.env['product.product'].browse(product)
+            res['value']['name'] = product_obj.name
+        return res
---
+++ project-addons/on_changes_without_code/models/sale.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = "sale.order.line"
+
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+                          uom=False, qty_uos=0, uos=False, name='',
+                          partner_id=False, lang=False, update_tax=True,
+                          date_order=False, packaging=False,
+                          fiscal_position=False, flag=False, context=None):
+        res = super(SaleOrderLine, self).\
+            product_id_change(cr, uid, ids, pricelist, product, qty=qty,
+                              uom=uom, qty_uos=qty_uos, uos=uos, name=name,
+                              partner_id=partner_id, lang=lang,
+                              update_tax=update_tax, date_order=date_order,
+                              packaging=packaging,
+                              fiscal_position=fiscal_position, flag=flag,
+                              context=context)
+        product_obj = self.pool.get('product.product')
+        if product and res.get('value') and res['value'].get('name'):
+            context_partner = context.copy()
+            context_partner.update({'lang': lang, 'partner_id': partner_id})
+            product_obj = product_obj.browse(cr, uid, product,
+                                             context=context_partner)
+            res['value']['name'] = product_obj.name
+        return res
