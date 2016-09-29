bb03af5c7e25f37f921085bdc1a5c7e69c0f6fc4
Omar <omarcs7r@gmail.com>
2016-09-29 14:09:14 +0200

    [IMP] carrier_api: Usamos decimal_precision en la definición del precio de las tarifas d envío

--- project-addons/carrier_api/models/delivery_carrier.py
+++ project-addons/carrier_api/models/delivery_carrier.py
@@ -3,8 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
+import openerp.addons.decimal_precision as dp
 
 
 class Carrier(models.Model):
     _inherit = 'delivery.carrier'
     service = fields.Many2one('carrier.api.service', 'Service')
+    normal_price = fields.\
+        Float('Normal Price', digits=dp.get_precision('Product Price'),
+              help="Keep empty if the pricing depends on the advanced "
+                   "pricing per destination")
