48edfdf412ab0f7768f648d26b0f6bbe8716ee16
jesusVMayor <jesus@comunitea.com>
2017-10-31 14:03:01 +0100

    [FIX]sale_store: Pequeña corrección.

--- project-addons/sale_store/__openerp__.py
+++ project-addons/sale_store/__openerp__.py
@@ -13,7 +13,7 @@
     "installable": True,
     "depends": [
         "sale_stock", "account_payment", "delivery",
-        "account_payment_partner"
+        "account_payment_partner", "bt_custom"
     ],
     "data": ['security/ir.model.access.csv',
              'views/layout.xml',
