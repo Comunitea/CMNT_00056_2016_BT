d3c71a2870e33134bfa9d95835887fa222200bcf
Omar <omarcs7r@gmail.com>
2017-11-06 12:27:42 +0100

    [FIX] sale_store: Corrección en la herencia del pie en las tiendas

--- project-addons/custom_commission/models/sale.py
+++ project-addons/custom_commission/models/sale.py
@@ -50,7 +50,6 @@ class SaleOrderLine(models.Model):
             name=name, partner_id=partner_id, lang=lang, update_tax=update_tax,
             date_order=date_order, packaging=packaging,
             fiscal_position=fiscal_position, flag=flag)
-
         if self.ids and self.agents and res['value'].get('agents'):
             del res['value']['agents']
 
