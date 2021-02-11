301925062e5be274b67ea22a8c91443b6eb4dde0
JesusVMayor <jesus@comunitea.com>
2021-02-11 23:40:53 +0100

    [FIX]alt_custom

--- project-addons/alt_custom/models/procurement_order.py
+++ project-addons/alt_custom/models/procurement_order.py
@@ -40,7 +40,7 @@ class ProcurementOrder(models.Model):
                 else:
                     available_draft_po_ids = po_obj.search(cr, uid, [
                         ('partner_id', '=', partner.id), ('state', '=', 'draft'), ('picking_type_id', '=', procurement.rule_id.picking_type_id.id),
-                        ('location_id', '=', procurement.location_id.id), ('company_id', '=', procurement.company_id.id), ('dest_address_id', '=', procurement.partner_dest_id.id), , ('procurement_group_id', '=', False)], context=context)
+                        ('location_id', '=', procurement.location_id.id), ('company_id', '=', procurement.company_id.id), ('dest_address_id', '=', procurement.partner_dest_id.id), ('procurement_group_id', '=', False)], context=context)
                 if available_draft_po_ids:
                     po_id = available_draft_po_ids[0]
                     po_rec = po_obj.browse(cr, uid, po_id, context=context)
