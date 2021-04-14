e6f6516f3fb86348108c22d930d2aa776fc249a3
JesusVMayor <jesus@comunitea.com>
2021-04-14 11:28:12 +0200

    [FIX]product_pack: Corregido error en herencias.

--- project-addons/product_pack/__openerp__.py
+++ project-addons/product_pack/__openerp__.py
@@ -37,7 +37,7 @@ as children of the pack product.
     """,
     "author": "Agile Business Group & NaN·tic",
     "website": "http://www.agilebg.com",
-    "depends": ["account", "sale", "sale_stock", "stock_account", "purchase"],
+    "depends": ["account", "sale", "sale_stock", "stock_account", "purchase", "stock_custom"],
     "data": ["security/ir.model.access.csv", "views/assets.xml", "pack_view.xml", "views/report_saleorder.xml"],
     "installable": True,
 }
