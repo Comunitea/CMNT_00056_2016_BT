e9ddaaf2121ab8ebbd857e88eaf4c3399f4a9e87
JesusVMayor <jesus@comunitea.com>
2021-10-21 17:54:26 +0200

    [FIX]stock_custom: Sobreescribimos valoración de inventario para solventar error con los quants.

--- project-addons/stock_custom/models/__init__.py
+++ project-addons/stock_custom/models/__init__.py
@@ -2,3 +2,4 @@
 # © 2019 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import stock
+from . import stock_valuation_history
---
+++ project-addons/stock_custom/models/stock_valuation_history.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# © 2021 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import models
+from openerp import tools
+
+
+class StockHistory(models.Model):
+    _inherit = 'stock.history'
+
+    def init(self, cr):
+        # Eliminaos la parte de los quants, y ponemos coste en el quant a 0, ya que no se utiliza.
+        tools.drop_view_if_exists(cr, 'stock_history')
+        cr.execute("""
+            CREATE OR REPLACE VIEW stock_history AS (
+              SELECT MIN(id) as id,
+                move_id,
+                location_id,
+                company_id,
+                product_id,
+                product_categ_id,
+                SUM(quantity) as quantity,
+                date,
+                0 as price_unit_on_quant,
+                source
+                FROM
+                ((SELECT
+                    stock_move.id AS id,
+                    stock_move.id AS move_id,
+                    dest_location.id AS location_id,
+                    dest_location.company_id AS company_id,
+                    stock_move.product_id AS product_id,
+                    product_template.categ_id AS product_categ_id,
+                    stock_move.product_uom_qty AS quantity,
+                    stock_move.date AS date,
+                    stock_move.origin AS source
+                FROM
+                    stock_move
+                JOIN
+                   stock_location dest_location ON stock_move.location_dest_id = dest_location.id
+                JOIN
+                    stock_location source_location ON stock_move.location_id = source_location.id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE stock_move.product_uom_qty>0 AND stock_move.state = 'done' AND dest_location.usage in ('internal', 'transit')
+                  AND (
+                    not (source_location.company_id is null and dest_location.company_id is null) or
+                    source_location.company_id != dest_location.company_id or
+                    source_location.usage not in ('internal', 'transit'))
+                ) UNION ALL
+                (SELECT
+                    (-1) * stock_move.id AS id,
+                    stock_move.id AS move_id,
+                    source_location.id AS location_id,
+                    source_location.company_id AS company_id,
+                    stock_move.product_id AS product_id,
+                    product_template.categ_id AS product_categ_id,
+                    - stock_move.product_uom_qty AS quantity,
+                    stock_move.date AS date,
+                    stock_move.origin AS source
+                FROM
+                    stock_move
+                JOIN
+                    stock_location source_location ON stock_move.location_id = source_location.id
+                JOIN
+                    stock_location dest_location ON stock_move.location_dest_id = dest_location.id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE stock_move.product_uom_qty>0 AND stock_move.state = 'done' AND source_location.usage in ('internal', 'transit')
+                 AND (
+                    not (dest_location.company_id is null and source_location.company_id is null) or
+                    dest_location.company_id != source_location.company_id or
+                    dest_location.usage not in ('internal', 'transit'))
+                ))
+                AS foo
+                GROUP BY move_id, location_id, company_id, product_id, product_categ_id, date, source
+            )""")
